e9b43cbae738a9b84306b8d685c5ec430f1719a6
limitlessza <65598056+limitlessza@users.noreply.github.com>
2022-10-26 19:17:14 +0200

    init Spring

---
+++ src/main/java/com/payroll/e/litepayroll/LitepayrollApplication.java
@@ -0,0 +1,13 @@
+package com.payroll.e.litepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitepayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitepayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payroll/e/litepayroll/LitepayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.e.litepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitepayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
